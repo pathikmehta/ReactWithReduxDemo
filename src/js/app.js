3e7ef073647821bcd118693c5b6605e822a15232
pathikmehta <pathikmehta11@gmail.com>
2016-10-12 13:25:33 +0530

    Async sample using promise middleware

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import { applyMiddleware, createStore } from "redux";
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import axios from 'axios';
+import promise from 'redux-promise-middleware';
 
 const initialState = {
 	fetching: false,
@@ -12,15 +13,15 @@ const initialState = {
 
 const reducer = function(state, action) {
 	switch(action.type) {
-		case 'FETCH_USERS_START': {
+		case 'FETCH_USERS_PENDING': {
 			return {...state, fetching: true};
 			break;
 		}
-		case 'FETCH_USERS_ERROR': {
+		case 'FETCH_USERS_REJECTED': {
 			return {...state, fetching: false, fetched: false, error: action.payload};
 			break;
 		}
-		case 'FETCH_USERS_COMPLETED': {
+		case 'FETCH_USERS_FULFILLED': {
 			return {...state, fetching: false, fetched: true, users: action.payload};
 			break;
 		}
@@ -32,13 +33,7 @@ const middleware = applyMiddleware(promise(), thunk, logger());
 
 const store = createStore(reducer, middleware);
 
-store.dispatch((dispatch) => {
-	dispatch({type: 'FETCH_USERS_START'});
-	axios.get("http://google.co.in").
-		then( (response) => {
-			dispacth({type: "FETCH_USERS_COMPLETED", payload: response.data});
-		})
-		.catch((err) => {
-			dispatch({type: 'FETCH_USERS_ERROR', payload: err})
-		})
+store.dispatch({
+	type: "FETCH_USERS",
+	payload: axios.get("http://google.co.in")
 });
